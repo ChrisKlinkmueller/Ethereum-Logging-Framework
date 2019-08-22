78fdaba9732c7bb2d78f6d96996989b4300a01c7
Chris <christopher.klinkmuller@csiro.au>
2019-08-22 16:20:08 +1000

    Initial Commit

---
+++ src/test/java/au/csiro/data61/aap/AppTest.java
@@ -0,0 +1,20 @@
+package au.csiro.data61.aap;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
