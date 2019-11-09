919a9440dc5674a69a9c19cdc1ed034954802594
Chris <christopher.klinkmuller@csiro.au>
2019-11-09 12:24:02 +1100

    MethodCallAnalyzer refactored: - now uses literal checks from AnalyzerUtils

--- src/main/java/au/csiro/data61/aap/parser/MethodCallAnalyzer.java
+++ src/main/java/au/csiro/data61/aap/parser/MethodCallAnalyzer.java
@@ -1,23 +1,12 @@
 package au.csiro.data61.aap.parser;
 
 import java.util.HashMap;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import au.csiro.data61.aap.library.Library;
-import au.csiro.data61.aap.parser.XbelParser.ArrayValueContext;
-import au.csiro.data61.aap.parser.XbelParser.LiteralContext;
 import au.csiro.data61.aap.parser.XbelParser.MethodCallContext;
 import au.csiro.data61.aap.parser.XbelParser.MethodParameterContext;
 import au.csiro.data61.aap.spec.Method;
-import au.csiro.data61.aap.spec.types.SolidityAddress;
-import au.csiro.data61.aap.spec.types.SolidityArray;
-import au.csiro.data61.aap.spec.types.SolidityBool;
-import au.csiro.data61.aap.spec.types.SolidityBytes;
-import au.csiro.data61.aap.spec.types.SolidityFixed;
-import au.csiro.data61.aap.spec.types.SolidityInteger;
-import au.csiro.data61.aap.spec.types.SolidityString;
 import au.csiro.data61.aap.spec.types.SolidityType;
 
 /**
@@ -25,32 +14,12 @@ import au.csiro.data61.aap.spec.types.SolidityType;
  */
 class MethodCallAnalyzer extends SemanticAnalyzer {
     private final VariableAnalyzer variableAnalyzer;
-    private final HashMap<Class<?>, BiPredicate<SolidityType, MethodParameterContext>> baseTypeChecks;
-    private final HashMap<Class<?>, Predicate<ArrayValueContext>> arrayTypeChecks;
     private final HashMap<String, Method> calledMethods;
 
     public MethodCallAnalyzer(ErrorCollector errorCollector, VariableAnalyzer variableAnalyzer) {
         super(errorCollector);
         assert variableAnalyzer != null;
         this.variableAnalyzer = variableAnalyzer;
-
-        this.baseTypeChecks = new HashMap<>();
-        this.baseTypeChecks.put(SolidityAddress.class, this::isAddress);
-        this.baseTypeChecks.put(SolidityBool.class, this::isBool);
-        this.baseTypeChecks.put(SolidityBytes.class, this::isBytes);
-        this.baseTypeChecks.put(SolidityFixed.class, this::isFixed);
-        this.baseTypeChecks.put(SolidityInteger.class, this::isInteger);
-        this.baseTypeChecks.put(SolidityString.class, this::isString);
-        this.baseTypeChecks.put(SolidityArray.class, this::isArray);
-
-        this.arrayTypeChecks = new HashMap<>();
-        this.arrayTypeChecks.put(SolidityAddress.class, this::isAddressArray);
-        this.arrayTypeChecks.put(SolidityBool.class, this::isBoolArray);
-        this.arrayTypeChecks.put(SolidityBytes.class, this::isBytesArray);
-        this.arrayTypeChecks.put(SolidityFixed.class, this::isFixedArray);
-        this.arrayTypeChecks.put(SolidityInteger.class, this::isIntegerArray);
-        this.arrayTypeChecks.put(SolidityString.class, this::isStringArray);
-
         this.calledMethods = new HashMap<>();
     }
 
@@ -100,96 +69,19 @@ class MethodCallAnalyzer extends SemanticAnalyzer {
             .allMatch(i -> this.areTypesMatching(method.getSignature().getParameterType(i), ctx.methodParameter(i)));
     }
 
-    private boolean areTypesMatching(SolidityType solType, MethodParameterContext paramType) {
-        final BiPredicate<SolidityType, MethodParameterContext> check = this.baseTypeChecks.get(solType.getClass());
-        return check != null && check.test(solType, paramType);
-    }    
-
-    private boolean isArray(SolidityType solType, MethodParameterContext ctx) {
+    
+    private boolean areTypesMatching(SolidityType varType, MethodParameterContext ctx) {
         if (ctx.variableReference() != null) {
-            return this.variableAnalyzer.existsVariable(ctx.variableReference().getText(), solType);
+            final SolidityType type = this.variableAnalyzer.getVariableType(ctx.variableReference().variableName().getText());
+            return type != null && varType.conceptuallyEquals(type);
         }
-
-        if (ctx.literal().arrayValue() != null) {
-            final SolidityType baseType = ((SolidityArray)solType);
-            final Predicate<ArrayValueContext> check = this.arrayTypeChecks.get(baseType.getClass());
-            final ArrayValueContext arrayCtx = ctx.literal().arrayValue();
-            return check != null && check.test(arrayCtx);
+        else if (ctx.literal() != null) {
+            return AnalyzerUtils.isTypeCompatible(varType, ctx.literal());
         }
-
-        return false;
-    }
-
-    private boolean isAddress(SolidityType solType, MethodParameterContext ctx) {
-        return this.hasType(ctx, solType, 
-        literalCtx -> literalCtx.BYTE_AND_ADDRESS_LITERAL() != null && AnalyzerUtils.isAddressLiteral(literalCtx.BYTE_AND_ADDRESS_LITERAL())
-        );
-    }
-
-    private boolean isAddressArray(ArrayValueContext ctx) {
-        return    ctx.byteAndAddressArrayValue() != null 
-               && ctx.byteAndAddressArrayValue().BYTE_AND_ADDRESS_LITERAL().stream().allMatch(AnalyzerUtils::isAddressLiteral);
-    }
-
-    private boolean isBool(SolidityType solType, MethodParameterContext ctx) {
-        return this.hasType(ctx, solType, literalCtx -> literalCtx.BOOLEAN_LITERAL() != null);
-    }
-
-    private boolean isBoolArray(ArrayValueContext ctx) {
-        return ctx.booleanArrayValue() != null;
-    }
-
-    private boolean isBytes(SolidityType solType, MethodParameterContext ctx) {
-        return this.hasType(
-            ctx, solType, 
-            literalCtx -> literalCtx.BYTE_AND_ADDRESS_LITERAL() != null && AnalyzerUtils.isBytesLiteral(literalCtx.BYTE_AND_ADDRESS_LITERAL())
-        );
-    }
-
-    private boolean isBytesArray(ArrayValueContext ctx) {
-        return    ctx.byteAndAddressArrayValue() != null
-               && ctx.byteAndAddressArrayValue().BYTE_AND_ADDRESS_LITERAL().stream().allMatch(AnalyzerUtils::isBytesLiteral);
-    }
-
-    private boolean isFixed(SolidityType solType, MethodParameterContext ctx) {
-        return this.hasType(ctx, solType, literalCtx -> literalCtx.FIXED_LITERAL() != null || literalCtx.INT_LITERAL() != null);
-    }
-
-    private boolean isFixedArray(ArrayValueContext ctx) {
-        return ctx.fixedArrayValue() != null;
-    }
-
-    private boolean isInteger(SolidityType solType, MethodParameterContext ctx) {
-        return this.hasType(ctx, solType, literalCtx -> literalCtx.INT_LITERAL() != null);
-    }
-
-    private boolean isIntegerArray(ArrayValueContext ctx) {
-        return ctx.intArrayValue() != null;
-    }
-
-    private boolean isString(SolidityType solType, MethodParameterContext ctx) {
-        return this.hasType(ctx, solType, literalCtx -> literalCtx.STRING_LITERAL() != null);
-    }
-
-    private boolean isStringArray(ArrayValueContext ctx) {
-        return ctx.stringArrayValue() != null;
-    }
-
-    private boolean hasType(
-        MethodParameterContext ctx, 
-        SolidityType type, 
-        Predicate<LiteralContext> literalCheck) {
-        
-        if (ctx.variableReference() != null) {
-            return this.variableAnalyzer.existsVariable(ctx.variableReference().getText(), type);
+        else {
+            throw new UnsupportedOperationException(String.format("This way of defining a parameter for a method call is not supported: '%s'.", ctx.getText()));
         }
-
-        if (ctx.literal() != null) {
-            return literalCheck.test(ctx.literal());
-        }
-
-        return false;
-    }
+    } 
 
     // TODO: this will be the place for emit and if conditions, as they will be mapped to 
 }
