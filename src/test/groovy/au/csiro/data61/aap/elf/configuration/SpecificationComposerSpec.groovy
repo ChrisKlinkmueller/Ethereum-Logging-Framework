8db80c1d0349b81dbffb2ed04843fc58baa335e7
Sai <42603306+yilin-sai@users.noreply.github.com>
2020-06-16 18:23:13 +1000

    add tests for SpecificationComposer

--- src/test/groovy/au/csiro/data61/aap/elf/configuration/SpecificationComposerSpec.groovy
+++ src/test/groovy/au/csiro/data61/aap/elf/configuration/SpecificationComposerSpec.groovy
@@ -1,15 +1,21 @@
 package au.csiro.data61.aap.elf.configuration
 
+import au.csiro.data61.aap.elf.core.Instruction
+import au.csiro.data61.aap.elf.core.VariableAssignment
+import au.csiro.data61.aap.elf.core.filters.TransactionFilter
+import au.csiro.data61.aap.elf.configuration.SpecificationComposer.FactoryState
 import spock.lang.Specification
 
 class SpecificationComposerSpec extends Specification {
     SpecificationComposer composer = new SpecificationComposer()
 
-    def "prepareBuild should throw when states not empty"() {
+    def "prepareProgramBuild should throw exception when states not empty"() {
         when: 'build a program with empty states'
         composer.prepareProgramBuild()
         then:
         notThrown(BuildException)
+        checkStates(composer, [FactoryState.PROGRAM])
+        checkInstructions(composer, [[]])
 
         when: 'build a program when another program is being build'
         composer.prepareProgramBuild()
@@ -18,7 +24,7 @@ class SpecificationComposerSpec extends Specification {
         e.getMessage() == 'A program can only be build when no other filter is being build.'
     }
 
-    def "prepareBuild should throw when current states not match"() {
+    def "prepareTransactionFilterBuild should throw exception when current states not match"() {
         given:
         composer.prepareProgramBuild()
 
@@ -33,5 +39,56 @@ class SpecificationComposerSpec extends Specification {
         composer.prepareTransactionFilterBuild()
         then:
         notThrown(BuildException)
+        checkStates(composer, [FactoryState.PROGRAM, FactoryState.BLOCK_RANGE_FILTER, FactoryState.TRANSACTION_FILTER])
+        checkInstructions(composer, [[], [], []])
+    }
+
+    def "buildTransactionFilter should build a transaction filter"() {
+        given:
+        composer.prepareProgramBuild()
+        composer.prepareBlockRangeBuild()
+        composer.prepareTransactionFilterBuild()
+
+        when:
+        composer.buildTransactionFilter(AddressListSpecification.ofAny(), AddressListSpecification.ofAny())
+
+        then:
+        checkStates(composer, [FactoryState.PROGRAM, FactoryState.BLOCK_RANGE_FILTER])
+        checkInstructions(composer, [[], [TransactionFilter]])
+    }
+
+    def "addInstruction should add an instruction"() {
+        when:
+        composer.prepareProgramBuild()
+        composer.prepareGenericFilterBuild()
+        then:
+        checkStates(composer, [FactoryState.PROGRAM, FactoryState.GENERIC_FILTER])
+        checkInstructions(composer, [[],[]])
+
+        when:
+        composer.addInstruction(ValueAssignmentSpecification.of(
+                ValueMutatorSpecification.ofVariableName("var"),
+                ValueAccessorSpecification.ofVariable("var")
+        ))
+        then:
+        checkStates(composer, [FactoryState.PROGRAM, FactoryState.GENERIC_FILTER])
+        checkInstructions(composer, [[],[VariableAssignment]])
+    }
+
+    static def checkStates(SpecificationComposer composer, List<FactoryState> states) {
+        Stack<FactoryState> stack = new Stack()
+        for (state in states) stack.push(state)
+        composer.states == stack
+    }
+
+    static def checkInstructions(SpecificationComposer composer, List<List<Class>> instructionTypes) {
+        def instructions = composer.instructions
+        assert instructions.size() == instructionTypes.size()
+        instructions.eachWithIndex { List<Instruction> entry, int i ->
+            assert entry.size() == instructionTypes[i].size()
+            entry.eachWithIndex { Instruction instruction, int j ->
+                assert instruction.getClass() == instructionTypes[i][j]
+            }
+        }
     }
 }
