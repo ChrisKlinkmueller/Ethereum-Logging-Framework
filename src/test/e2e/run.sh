ee4fd4b4b8f7d77ade4929ed27fc38b67dce9762
Mykola Digtiar <mykola.digtiar@gmail.com>
2021-01-07 17:28:33 +0100

    #83 - fix shell warnings and define constants for dirs

--- src/test/e2e/run.sh
+++ src/test/e2e/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 function colorecho() {
-	echo -e "\033[01;32m === $@ === \033[00m"
+	echo -e "\033[01;32m === $* === \033[00m"
 }
 
 function redecho() {
-	echo -e "\033[01;31m === $@ === \033[00m"
+	echo -e "\033[01;31m === $* === \033[00m"
 }
 
 function main() {
-	if [ -d 'extracted' ]; then
+	if [ -d "$ED" ]; then
 		if [ ! "$1" = "$AUTO_PARAM" ] && [ ! "$2" = "$AUTO_PARAM" ] && [ ! "$3" = "$AUTO_PARAM" ]; then
 			echo "folder 'extracted' already exists! do you want to delete it and start again? (y/n)"
 			read -r confirm
@@ -18,50 +18,47 @@ function main() {
 				exit 1
 			else
 				colorecho "Cleaning up old test environment"
-				rm -rf extracted
+				rm -rf "$ED"
 			fi
 		else
 			colorecho "Cleaning up old test environment"
-			rm -rf extracted
+			rm -rf "$ED"
 		fi
 	fi
 
 	colorecho "Setting up test environment"
-	mkdir -p extracted/{AugurContractRegistry,CryptoKitties,NetworkStatistics,Rebesky_Augur,Rebesky_ChickenHunt,Rebesky_Idex1}
+	mkdir -p "$ED"/{AugurContractRegistry,CryptoKitties,NetworkStatistics,Rebesky_Augur,Rebesky_ChickenHunt,Rebesky_Idex1}
 
-	echo "$MAN_AugurContractRegistry" > extracted/AugurContractRegistry.bcql
-	echo "$MAN_CryptoKitties" > extracted/CryptoKitties.bcql
-	echo "$MAN_NetworkStatistics" > extracted/NetworkStatistics.bcql
-	echo "$MAN_Rebesky_Augur" > extracted/Rebesky_Augur.bcql
-	echo "$MAN_Rebesky_ChickenHunt" > extracted/Rebesky_ChickenHunt.bcql
-	echo "$MAN_Rebesky_Idex1" > extracted/Rebesky_Idex1.bcql
+	echo "$MAN_AugurContractRegistry" > "$ED"/AugurContractRegistry.bcql
+	echo "$MAN_CryptoKitties" > "$ED"/CryptoKitties.bcql
+	echo "$MAN_NetworkStatistics" > "$ED"/NetworkStatistics.bcql
+	echo "$MAN_Rebesky_Augur" > "$ED"/Rebesky_Augur.bcql
+	echo "$MAN_Rebesky_ChickenHunt" > "$ED"/Rebesky_ChickenHunt.bcql
+	echo "$MAN_Rebesky_Idex1" > "$ED"/Rebesky_Idex1.bcql
 
-	touch extracted/AugurContractRegistry/error.log.xelf
-	touch extracted/CryptoKitties/error.log.xelf
-	touch extracted/NetworkStatistics/error.log.xelf
-	touch extracted/Rebesky_Augur/error.log.xelf
-	touch extracted/Rebesky_ChickenHunt/error.log.xelf
-	touch extracted/Rebesky_Idex1/error.log.xelf
+	touch "$ED"/AugurContractRegistry/error.log.xelf
+	touch "$ED"/CryptoKitties/error.log.xelf
+	touch "$ED"/NetworkStatistics/error.log.xelf
+	touch "$ED"/Rebesky_Augur/error.log.xelf
+	touch "$ED"/Rebesky_ChickenHunt/error.log.xelf
+	touch "$ED"/Rebesky_Idex1/error.log.xelf
 
-	echo "$XELF_AugurContractRegistry" > extracted/AugurContractRegistry/all.log.xelf
-	echo "$XELF_CryptoKitties" > extracted/CryptoKitties/log_pid0_all.xes.xelf
-	echo "$XELF_NetworkStatistics" > extracted/NetworkStatistics/NetworkStatistics_all.csv.xelf
-	echo "$XELF_Rebesky_Augur" > extracted/Rebesky_Augur/log_pid0_all.xes.xelf
-	echo "$XELF_Rebesky_ChickenHunt" > extracted/Rebesky_ChickenHunt/log_pid0_all.xes.xelf
-	echo "$XELF_Rebesky_Idex1" > extracted/Rebesky_Idex1/Idex_calls_all.csv.xelf
+	echo "$XELF_AugurContractRegistry" > "$ED"/AugurContractRegistry/all.log.xelf
+	echo "$XELF_CryptoKitties" > "$ED"/CryptoKitties/log_pid0_all.xes.xelf
+	echo "$XELF_NetworkStatistics" > "$ED"/NetworkStatistics/NetworkStatistics_all.csv.xelf
+	echo "$XELF_Rebesky_Augur" > "$ED"/Rebesky_Augur/log_pid0_all.xes.xelf
+	echo "$XELF_Rebesky_ChickenHunt" > "$ED"/Rebesky_ChickenHunt/log_pid0_all.xes.xelf
+	echo "$XELF_Rebesky_Idex1" > "$ED"/Rebesky_Idex1/Idex_calls_all.csv.xelf
 
   if [ ! "$1" = "$SKIP_BUILD_PARAM" ] && [ ! "$2" = "$SKIP_BUILD_PARAM" ] && [ ! "$3" = "$SKIP_BUILD_PARAM" ]; then
 	  colorecho "Building the BLF"
-    WD="$SCRIPT_DIR/../../../"
     cd "$WD" || { redecho "Cannot cd into '$WD'. Probably the specified directory does not exist..." ; exit 2; }
     mvn package || { redecho "Bulding the BLF failed!"; exit 2; }
   fi
 
-	# JAR=$(find $WD -iname "blf-*.jar" | grep -v "javadoc")
-	JAR="$SCRIPT_DIR/../../../target/elf-cmd.jar"
-	echo "$JAR"
+	cd "$ED" || { redecho "Cannot cd into '$ED'. Probably the specified directory does not exist..." ; exit 2; }
 
-	cd "$WD/extracted" || { redecho "Cannot cd into '$WD/extracted'. Probably the specified directory does not exist..." ; exit 2; }
+  echo "JAR file should be located at '$JAR'"
 
 	colorecho "Testing AugurContractRegistry"
   if [ "$1" = "$SILENT_PARAM" ] || [ "$2" = "$SILENT_PARAM" ] || [ "$3" = "$SILENT_PARAM" ]; then
@@ -130,7 +127,7 @@ function main() {
 		read -r confirm
 		if [ "$confirm" = "y" ]; then
 			echo "Deleting generated data"
-			rm -rf extracted
+			rm -rf "$ED"
 		fi
 		echo "Run mvn clean? (y/n)"
 		read -r confirm
@@ -140,7 +137,7 @@ function main() {
 		fi
 	else
 		echo "Deleting generated data"
-		rm -rf extracted
+		rm -rf "$ED"
 	fi
 
 	colorecho "Goodbye"
@@ -151,31 +148,34 @@ SKIP_BUILD_PARAM="--auto"
 SILENT_PARAM="--silent"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WD="$SCRIPT_DIR"/../../..
+ED="$WD"/extracted
+JAR="$WD"/target/elf-cmd.jar
 
 # BCQL scripts
-MAN_AugurContractRegistry=$(cat "$SCRIPT_DIR/bcql/AugurContractRegistry.bcql")
+MAN_AugurContractRegistry=$(cat "$SCRIPT_DIR"/bcql/AugurContractRegistry.bcql)
 
-MAN_CryptoKitties=$(cat "$SCRIPT_DIR/bcql/CryptoKitties.bcql")
+MAN_CryptoKitties=$(cat "$SCRIPT_DIR"/bcql/CryptoKitties.bcql)
 
-MAN_NetworkStatistics=$(cat "$SCRIPT_DIR/bcql/NetworkStatistics.bcql")
+MAN_NetworkStatistics=$(cat "$SCRIPT_DIR"/bcql/NetworkStatistics.bcql)
 
-MAN_Rebesky_Augur=$(cat "$SCRIPT_DIR/bcql/Rebesky_Augur.bcql")
+MAN_Rebesky_Augur=$(cat "$SCRIPT_DIR"/bcql/Rebesky_Augur.bcql)
 
-MAN_Rebesky_ChickenHunt=$(cat "$SCRIPT_DIR/bcql/Rebesky_ChickenHunt.bcql")
+MAN_Rebesky_ChickenHunt=$(cat "$SCRIPT_DIR"/bcql/Rebesky_ChickenHunt.bcql)
 
-MAN_Rebesky_Idex1=$(cat "$SCRIPT_DIR/bcql/Rebesky_Idex1.bcql")
+MAN_Rebesky_Idex1=$(cat "$SCRIPT_DIR"/bcql/Rebesky_Idex1.bcql)
 
 # BCQL expected outputs
-XELF_AugurContractRegistry=$(cat "$SCRIPT_DIR/outputs/AugurContractRegistry.o")
+XELF_AugurContractRegistry=$(cat "$SCRIPT_DIR"/outputs/AugurContractRegistry.o)
 
-XELF_CryptoKitties=$(cat "$SCRIPT_DIR/outputs/CryptoKitties.o")
+XELF_CryptoKitties=$(cat "$SCRIPT_DIR"/outputs/CryptoKitties.o)
 
-XELF_NetworkStatistics=$(cat "$SCRIPT_DIR/outputs/NetworkStatistics.o")
+XELF_NetworkStatistics=$(cat "$SCRIPT_DIR"/outputs/NetworkStatistics.o)
 
-XELF_Rebesky_Augur=$(cat "$SCRIPT_DIR/outputs/Rebesky_Augur.o")
+XELF_Rebesky_Augur=$(cat "$SCRIPT_DIR"/outputs/Rebesky_Augur.o)
 
-XELF_Rebesky_ChickenHunt=$(cat "$SCRIPT_DIR/outputs/Rebesky_ChickenHunt.o")
+XELF_Rebesky_ChickenHunt=$(cat "$SCRIPT_DIR"/outputs/Rebesky_ChickenHunt.o)
 
-XELF_Rebesky_Idex1=$(cat "$SCRIPT_DIR/outputs/Rebesky_Idex1.o")
+XELF_Rebesky_Idex1=$(cat "$SCRIPT_DIR"/outputs/Rebesky_Idex1.o)
 
 main "$@"
