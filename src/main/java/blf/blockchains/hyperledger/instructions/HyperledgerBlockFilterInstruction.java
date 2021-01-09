ea4df8e77ccafac98c659a11639455aa7039fad7
Mykola Digtiar <mykola.digtiar@gmail.com>
2021-01-09 12:49:02 +0100

    #79 - change comments in HyperledgerBlockFilterInstruction

--- src/main/java/blf/blockchains/hyperledger/instructions/HyperledgerBlockFilterInstruction.java
+++ src/main/java/blf/blockchains/hyperledger/instructions/HyperledgerBlockFilterInstruction.java
@@ -11,7 +11,7 @@ import java.util.LinkedList;
 import java.util.logging.Logger;
 
 /**
- * This class handles the `BLOCKS (fromBlock) (toBlock)` filter of the .bcql file.
+ * This class handles the 'BLOCKS (fromBlock) (toBlock)' filter of the .bcql file.
  */
 public class HyperledgerBlockFilterInstruction extends FilterInstruction {
 
