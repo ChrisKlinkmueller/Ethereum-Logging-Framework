0eb94535ac5fd829c0a4feef2521696f1b0c29a2
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-01-11 23:23:12 +0100

    #82 - add javadoc comments

--- src/main/java/blf/blockchains/hyperledger/instructions/HyperledgerLogEntryFilterInstruction.java
+++ src/main/java/blf/blockchains/hyperledger/instructions/HyperledgerLogEntryFilterInstruction.java
@@ -15,7 +15,9 @@ import java.math.BigInteger;
 import java.util.List;
 
 /**
- *
+ * HyperledgerLogEntryFilterInstruction is an Instruction for the Hyperledger log extraction mode of the Blockchain
+ * Logging Framework. It extracts the requested event from the current Block and stores the extracted parameter values
+ * in the ProgramState.
  */
 public class HyperledgerLogEntryFilterInstruction implements Instruction {
 
@@ -25,6 +27,13 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
     private final String eventName;
     private final List<Pair<String, String>> entryParameters;
 
+    /**
+     * Constructs a HyperledgerLogEntryFilterInstruction.
+     *
+     * @param addressNames The list of all chaincode addresses the user requested in the manifest.
+     * @param eventName The name of the event the user requested to extract.
+     * @param entryParameters The list of parameter types and names the event contains.
+     */
     public HyperledgerLogEntryFilterInstruction(
         final List<String> addressNames,
         String eventName,
@@ -37,6 +46,13 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         this.exceptionHandler = new ExceptionHandler();
     }
 
+    /**
+     * execute is called once the program is constructed from the manifest. It contains the logic for extracting an
+     * event from the Hyperledger block that the BLF is currently analyzing. It is called by the Program class.
+     *
+     * @param state The current ProgramState of the BLF, provided by the Program when called.
+     * @throws ProgramException never explicitly
+     */
     @Override
     public void execute(ProgramState state) throws ProgramException {
         HyperledgerProgramState hyperledgerProgramState = (HyperledgerProgramState) state;
@@ -64,6 +80,15 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         }
     }
 
+    /**
+     * Tries to parse the ChaincodeEvent in the manner the manifest requested.
+     * <p>
+     * Firstly, it tries to parse the payload as a JSON string. If that fails, it tries to extract a single parameter
+     * from the unstructured payload.
+     *
+     * @param hyperledgerProgramState The current ProgramState of the BLF.
+     * @param ce The current ChaincodeEvent to analyze.
+     */
     private void parseChaincodeEvent(HyperledgerProgramState hyperledgerProgramState, ChaincodeEvent ce) {
         // first try parse json
         String payloadString = new String(ce.getPayload());
@@ -78,6 +103,16 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         }
     }
 
+    /**
+     * Tries to parse an event from a nested JSON object, if another JSON object is contained in the top-level
+     * JSON object of the payload and its key is the requested event name.
+     * If such object is not found, the top-level JSON object is tried to be handles as a flat JSON object containing
+     * the event parameters.
+     *
+     * @param hyperledgerProgramState The current ProgramState of the BLF.
+     * @param ce The ChaincodeEvent to analyze.
+     * @param obj The top-level JSON object that was successfully parsed from the event payload.
+     */
     private void parseNestedJsonPayload(HyperledgerProgramState hyperledgerProgramState, ChaincodeEvent ce, JSONObject obj) {
         try {
             // if json is nested, search for eventName as index in the nested object
@@ -96,6 +131,13 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         }
     }
 
+    /**
+     * Tries to parse the requested event from a flat JSON object, if the event name fits the requested event name.
+     *
+     * @param hyperledgerProgramState The current ProgramState of the BLF.
+     * @param ce The ChaincodeEvent to analyze.
+     * @param obj The top-level JSON object that was successfully parsed from the event payload.
+     */
     private void parseFlatJsonPayload(HyperledgerProgramState hyperledgerProgramState, ChaincodeEvent ce, JSONObject obj) {
         // if json is flat, check if the eventName is the event name of the Event
         if (this.eventName.equals(ce.getEventName())) {
@@ -109,6 +151,18 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         }
     }
 
+    /**
+     * Tries to parse a single parameter from an unstructured payload.
+     * <p>
+     * If the event payload does not contain structured JSON data, we can not now if it holds any other structure of
+     * multiple parameters internally, as all we see is an array of bytes. We can try to parse one single parameter out
+     * of these bytes.
+     *
+     * @param hyperledgerProgramState The current ProgramState of the BLF.
+     * @param ce The ChaincodeEvent to analyze.
+     * @param payloadString The payload of the ce, already converted to a string.
+     * @param jsonException The exception thrown by the JSON parser, the reason why the JSON could not be recognized.
+     */
     private void parseNonStructuredPayload(
         HyperledgerProgramState hyperledgerProgramState,
         ChaincodeEvent ce,
@@ -131,6 +185,16 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         }
     }
 
+    /**
+     * Tries to parse the payload into a type hinted by parameterType. Afterwards sets it to the ValueStore of the
+     * ProgramState.
+     *
+     * @param hyperledgerProgramState The current ProgramState of the BLF.
+     * @param parameterName The name of the parameter that is to be extracted and set to the state.
+     * @param parameterType The type of the parameter that is to be extracted and set to the state.
+     * @param payloadString The payload of the ce, already converted to a string.
+     * @param payload The payload of the ce, as the original array of bytes.
+     */
     private void setStateValue(
         HyperledgerProgramState hyperledgerProgramState,
         String parameterName,
@@ -166,6 +230,15 @@ public class HyperledgerLogEntryFilterInstruction implements Instruction {
         }
     }
 
+    /**
+     * Tries to parse the contents of a JSON object into a type hinted by parameterType. Afterwards sets it to the
+     * ValueStore of the ProgramState.
+     *
+     * @param hyperledgerProgramState The current ProgramState of the BLF.
+     * @param parameterName The name of the parameter that is to be extracted and set to the state.
+     * @param parameterType The type of the parameter that is to be extracted and set to the state.
+     * @param obj The JSON object containing the requested event.
+     */
     private void setStateValue(
         HyperledgerProgramState hyperledgerProgramState,
         String parameterName,
