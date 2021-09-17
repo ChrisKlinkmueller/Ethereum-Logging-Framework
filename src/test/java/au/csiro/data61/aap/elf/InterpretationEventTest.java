53c10f7e93273cd7ad3427e79748aad659cb9fa5
Klinkmueller, Christopher (Data61, Eveleigh) <Christopher.Klinkmueller@data61.csiro.au>
2021-09-17 15:23:58 +1000

    Refactors tests of constructor

--- src/test/java/au/csiro/data61/aap/elf/InterpretationEventTest.java
+++ src/test/java/au/csiro/data61/aap/elf/InterpretationEventTest.java
@@ -30,52 +30,62 @@ class InterpretationEventTest {
     }
 
     @ParameterizedTest
-    @MethodSource
-    void constructorsIntializeCorrectly(Type type, Token token, String message, Throwable cause) {
-        InterpretationEvent event = new InterpretationEvent(type, message);
-        assertEquals(type, event.getType());
-        assertEquals(InterpretationEvent.DEFAULT_LINE, event.getLine());
-        assertEquals(InterpretationEvent.DEFAULT_COLUMN, event.getColumn());
-        assertEquals(message, event.getMessage());
-        assertNull(event.getCause());
-
-        event = new InterpretationEvent(type, message, cause);
-        assertEquals(type, event.getType());
-        assertEquals(InterpretationEvent.DEFAULT_LINE, event.getLine());
-        assertEquals(InterpretationEvent.DEFAULT_COLUMN, event.getColumn());
-        assertEquals(message, event.getMessage());
-        assertEquals(cause, event.getCause());
-
-        event = new InterpretationEvent(type, token, message);
-        assertEquals(type, event.getType());
-        assertEquals(token.getLine(), event.getLine());
-        assertEquals(token.getCharPositionInLine(), event.getColumn());
-        assertEquals(message, event.getMessage());
-        assertNull(event.getCause());
-
-        event = new InterpretationEvent(type, token, message, cause);
-        assertEquals(type, event.getType());
-        assertEquals(token.getLine(), event.getLine());
-        assertEquals(token.getCharPositionInLine(), event.getColumn());
-        assertEquals(message, event.getMessage());
-        assertEquals(cause, event.getCause());
-
-        event = new InterpretationEvent(type, token.getLine(), token.getCharPositionInLine(), message);
-        assertEquals(type, event.getType());
-        assertEquals(token.getLine(), event.getLine());
-        assertEquals(token.getCharPositionInLine(), event.getColumn());
-        assertEquals(message, event.getMessage());
-        assertNull(event.getCause());
-
-        event = new InterpretationEvent(type, token.getLine(), token.getCharPositionInLine(), message, cause);
-        assertEquals(type, event.getType());
-        assertEquals(token.getLine(), event.getLine());
-        assertEquals(token.getCharPositionInLine(), event.getColumn());
-        assertEquals(message, event.getMessage());
-        assertEquals(cause, event.getCause());
+    @MethodSource("constructorTestCases")
+    void testTypeMessageConstructor(Type type, Token token, String message, Throwable cause) {
+        final InterpretationEvent event = new InterpretationEvent(type, message);
+        this.verifyEvent(event, type, InterpretationEvent.DEFAULT_LINE, InterpretationEvent.DEFAULT_COLUMN, message, null);
     }
 
-    static Stream<Arguments> constructorsIntializeCorrectly() {
+    @ParameterizedTest
+    @MethodSource("constructorTestCases")
+    void testTypeMessageCauseConstructor(Type type, Token token, String message, Throwable cause) {
+        final InterpretationEvent event = new InterpretationEvent(type, message, cause);
+        this.verifyEvent(event, type, InterpretationEvent.DEFAULT_LINE, InterpretationEvent.DEFAULT_COLUMN, message, cause);
+    }
+
+    @ParameterizedTest
+    @MethodSource("constructorTestCases")
+    void testTypeTokenMessageConstructor(Type type, Token token, String message, Throwable cause) {
+        final InterpretationEvent event = new InterpretationEvent(type, token, message);
+        this.verifyEvent(event, type, token.getLine(), token.getCharPositionInLine(), message, null);
+    }
+
+    @ParameterizedTest
+    @MethodSource("constructorTestCases")
+    void testTypeTokenMessageCauseConstructor(Type type, Token token, String message, Throwable cause) {
+        final InterpretationEvent event = new InterpretationEvent(type, token, message, cause);
+        this.verifyEvent(event, type, token.getLine(), token.getCharPositionInLine(), message, cause);
+    }
+
+    @ParameterizedTest
+    @MethodSource("constructorTestCases")
+    void testTypePosMessageConstructor(Type type, Token token, String message, Throwable cause) {
+        final InterpretationEvent event = new InterpretationEvent(type, token.getLine(), token.getCharPositionInLine(), message);
+        this.verifyEvent(event, type, token.getLine(), token.getCharPositionInLine(), message, null);
+    }
+
+    @ParameterizedTest
+    @MethodSource("constructorTestCases")
+    void testTypePosMessageCauseConstructor(Type type, Token token, String message, Throwable cause) {
+        final InterpretationEvent event = new InterpretationEvent(type, token.getLine(), token.getCharPositionInLine(), message, cause);
+        this.verifyEvent(event, type, token.getLine(), token.getCharPositionInLine(), message, cause);
+    }
+
+    private void verifyEvent(InterpretationEvent event, Type type, int line, int col, String message, Throwable cause) {
+        assertEquals(type, event.getType());
+        assertEquals(line, event.getLine());
+        assertEquals(col, event.getColumn());
+        assertEquals(message, event.getMessage());
+
+        if (cause == null) {
+            assertNull(event.getCause());
+        }
+        else {
+            assertEquals(cause, event.getCause());
+        }
+    }
+
+    static Stream<Arguments> constructorTestCases() {
         return Stream.of(
             Arguments.of(Type.ERROR, mock(Token.class), "An error that occurred during parsing.", new IllegalArgumentException()),
             Arguments.of(Type.INFO, mock(Token.class), "An info regarding the analysis.", new UnsupportedOperationException()),
