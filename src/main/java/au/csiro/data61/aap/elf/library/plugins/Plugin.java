808c2d382833ea4c570bc0dd6d42675b731d68c8
Klinkmueller, Christopher (Data61, Eveleigh) <Christopher.Klinkmueller@data61.csiro.au>
2021-10-01 10:36:56 +1000

    Changes parsing API for plugins

--- src/main/java/au/csiro/data61/aap/elf/library/plugins/Plugin.java
+++ src/main/java/au/csiro/data61/aap/elf/library/plugins/Plugin.java
@@ -3,6 +3,8 @@ package au.csiro.data61.aap.elf.library.plugins;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import au.csiro.data61.aap.elf.grammar.EthqlParser.PluginStatementContext;
+
 public abstract class Plugin {
     private final String name;
 
@@ -16,11 +18,10 @@ public abstract class Plugin {
         return name;
     }
 
-    public PluginParseResult parseAction(PluginAction action, String code) {
-        checkNotNull(action);
-        checkNotNull(code);
-        return this.parse(action, code);
+    public PluginParseResult parseAction(PluginStatementContext ctx) {
+        checkNotNull(ctx);
+        return this.parse(ctx);
     }
 
-    protected abstract PluginParseResult parse(PluginAction action, String code);
+    protected abstract PluginParseResult parse(PluginStatementContext ctx);
 }
